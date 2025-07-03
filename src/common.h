132bc3db45d71020017028f4ad1789914f6ae46d
aguswesly <agustchannel@gmail.com>
2025-07-03 09:58:42 +0700

    feat: disable debug on playground

--- src/common.h
+++ src/common.h
@@ -1,18 +1,22 @@
 #ifndef ws_common_h
 #define ws_common_h
-#include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
 #include <assert.h>
-#include <stdbool.h>
 #include <ctype.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+#define ENABLE_GC
 #define NAN_BOXING
+
+#ifndef __EMSCRIPTEN__
+
 #define DEBUG_TRACE_EXECUTION
+// #define TEST_STRESS_GC
+// #define DEBUG_GC
 // #define DEBUG_PRINT
 
-// #define TEST_STRESS_GC
-#define ENABLE_GC
-// #define DEBUG_GC
+#endif // __EMSCRIPTEN__
 
 #endif // !ws_common_h
